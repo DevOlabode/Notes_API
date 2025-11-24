13099c27f6867192f3890990a5a950fa233241c9
Samuel Olabode <Solabode499@gmail.com>
2025-11-23 19:02:18 -0700

    `Removed required field from noteSchema title`

--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const noteSchema = Joi.object({
-  title: Joi.string().min(1).max(100).required(),
+  title: Joi.string().min(1).max(100),
   content: Joi.string().min(1).required(),
   tags: Joi.array().items(Joi.string().max(30)).required(),
   isArchived: Joi.boolean().optional(),
